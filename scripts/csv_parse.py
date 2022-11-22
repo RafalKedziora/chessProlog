d4ef76e3d4eb892837bed73d01561d389fdc010f
RKędziora <kontakt.rkedziora@gmail.com>
2022-11-22 21:12:04 +0100

    refactor csv_parser

--- scripts/csv_parse.py
+++ scripts/csv_parse.py
@@ -1,37 +1,49 @@
-def goodFormat(data):
-    commaMode = True
-    commaNum = 0
-    newString = ""
-    for x in data:
-        if x == "(":
-            newString += x
-            commaMode = False
-        elif x == ")":
-            newString += x
-            commaMode = True
-        elif x == ",":
-            if commaMode:
-                commaNum += 1 
-                newString += ";"
+import os
+import sys
+import pandas as pd
+
+comma_mode = True
+def choose_sign(sign):
+    global comma_mode
+    match sign:
+        case "(":
+            comma_mode = False
+            return sign
+        case ")":
+            comma_mode = True
+            return sign
+        case ",":
+            if comma_mode:
+                return sign
             else:
-                newString += x
-        elif x == "\n":
-            pass
-        else:
-            newString += x
-        if commaNum == 8:
-            newString += "\n"
-            commaNum = 0
-    return newString
+                return ";"
+        case "\n":
+            return ''
+        case _:
+            return sign
+
+def string_builder(data):
+    formatted_data = ""
+    comma_num = 0
+    for x in data:
+        new_char = choose_sign(x)
+        formatted_data += new_char
+        if(new_char == ","):
+            comma_num += 1
+            if(comma_num == 8):
+                formatted_data += "\n"
+                comma_num = 0
+    return formatted_data
             
         
 
 def main():
-    with open("dopytona.txt", "r+") as file:
-        data = file.read()
+    csv_type_data = string_builder(sys.argv[1])
+    
+    with open("results.csv", "w") as fileWriter:
+        fileWriter.write(csv_type_data)
 
-    newString = goodFormat(data)
-
-    with open("nalejkurwiemiodu.csv", "w") as fileWriter:
-        fileWriter.write(newString)
+    read = pd.read_csv('results.csv')
+    read.to_excel('results.xlsx', header=False, index=False)
+    os.remove('results.csv')
 main()
